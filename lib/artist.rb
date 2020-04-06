c67fb74addadb8db6f35fdc168947352fcf913fa
Yingqi Chen <chanwk18@hotmail.com>
2020-04-06 17:30:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,10 +22,7 @@ def self.all
 end
 
 def self.find_or_create_by_name(name)
-  if @@all.any?{|artist|artist.name==name}
-    @@all.find{|artist|artist.name==name}
-  else self.new(name)
-  end
+  @@all.any?{|artist|artist.name==name}?  @@all.find{|artist|artist.name==name} : self.new(name)
 end
 
 def print_songs
